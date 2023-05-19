a83550e452f0d24f31ce5e53fffb20011d5f7dd6
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-19 15:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function getSize(){
     let width = document.documentElement.clientWidth;
     let height = document.documentElement.clientHeight;
     let div = document.querySelector("#sizeInfo");
-    div.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+    div.innerHTML = `<h1> Width: ${width} and Height: ${height} </h1>`;
 }
